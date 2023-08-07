7d166e1e7d5aa25385ad50fbc76106952132629c
WanjikuN <cikunjoroge4@gmail.com>
2023-08-07 22:32:58 +0300

    Update React App without JSX

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ const rootElement = document.getElementById('root');
 
 
 // loop through an array
-const ints= [9,2,3];
-ints.forEach(i=>{
-    let li =document.createElement('li');
-    li.innerHTML = i;
-    rootElement.appendChild(li);
-})
+// const ints= [9,2,3];
+// ints.forEach(i=>{
+//     let li =document.createElement('li');
+//     li.innerHTML = i;
+//     rootElement.appendChild(li);
+// })
+
+// Create react virtual DOM root
+// assign it to a variable
+const root = ReactDOM.createRoot(rootElement);
+const ints= [8,2,3];
+const childrenElements = [];
+childrenElements.push(
+    React.createElement('li',{key: ints[0]},ints[0])
+)
+childrenElements.push(
+    React.createElement('li',{key: ints[1]},ints[1])
+)
+childrenElements.push(
+    React.createElement('li',{key: ints[2]},ints[2])
+)
+root.render(childrenElements);
 };
